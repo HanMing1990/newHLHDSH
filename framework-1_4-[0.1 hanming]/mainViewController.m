dc117f8ebea86b77e42761de7235b07f51df675f
HanMing1990 <hanm13@mails.tsinghua.edu.cn>
2016-01-10 11:30:14 +0800

    after he

--- framework-1_4-[0.1 hanming]/mainViewController.m
+++ framework-1_4-[0.1 hanming]/mainViewController.m
@@ -25,7 +25,7 @@
     [super viewDidLoad];
     
     // Do any additional setup after loading the view.
-    //this is a test line
+    //this is second line
     //滑动手势
     self.leftSwipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipes:)];
     self.rightSwipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipes:)];
